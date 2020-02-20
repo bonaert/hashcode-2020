eeaede15055c10ad790c8de72c41b217bfde1e2c
bonaert <harokb@gmail.com>
2020-02-20 19:45:58 +0100

    Small bugfix + pseudocode

---
+++ src/pseudocode.py
@@ -0,0 +1,61 @@
+books_already_scanned = set()
+current_time = 0
+remaining_libraries = [0, 1, 2, 3]
+solution = []
+
+
+def get_score_and_books(library, books_already_scanned, remaining_days):
+    # Compute how many books we can scan
+    unscanned_books = library.books - books_already_scanned
+    max_books_we_can_scan = min(
+        len(unscanned_books),
+        library.scanning_speed * remaining_days
+    )
+
+    # Pick which books we will scan. Balance between picking the highest value books and the books
+    # that are unique to this library
+
+    # V1: pick the highest value books (very basic, very dumb)
+    # TODO: be smarter than this
+    scores_and_ids = [(book.score, book.id) for book in unscanned_books]
+    chosen_scores_and_ids = sorted(scores_and_ids)[:max_books_we_can_scan]
+
+    best_books, total_score = [], 0
+    for (book_score, book_id) in chosen_scores_and_ids:
+        best_books.append(book_id)
+        total_score += book_score
+
+    return best_books, total_score
+
+
+
+"""
+Current approach:
+While there's still time, pick the library we're opening and the books we'll scan there
+
+Other approach:
+Look at it one day at the time. Each day, pick which books we will scan and, if it's possible, which library we will open.
+"""
+
+while current_time < maxTime:
+
+    # Choisir la bibliothèque qu'on va lire
+    best_score, best_library, best_books_to_scan = -1, -1, []
+    for library in remaining_libraries:
+        score, books_to_scan = get_score_and_books(library, books_already_scanned, maxTime - current_time - signup_time)
+        if score > best_score:
+            best_library = library
+            best_score = score
+            best_books_to_scan = books_to_scan
+
+    books_already_scanned.add(best_books_to_scan)
+
+    solution.append(
+        LibraryScan(
+            best_library,
+            current_time,
+            best_books_to_scan
+        )
+    )
+
+    current_time += library.sign_up_time
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,38 @@
-from collections import namedtuple
+from dataclasses import dataclass
+from typing import Set, List, Tuple
 
-Librairy = namedtuple("Librairy", ["id", "sign_up_time", "ship_per_day", "books"])
+
+@dataclass
+class Library:
+    id: int
+    sign_up_time: int
+    ship_per_day: int
+    books: Set[int]
 
 
-def read_input(filename):
-    librairies = []
+def read_input(filename: str) -> Tuple[List[Library], List[int]]:
+    libraries = []
     with open(filename, "r") as f:
         lines = f.readlines()
-        num_librairies = int(lines[0].split(" ")[1])
-        book_scores = lines[1].strip().split(" ")
+        num_libraries = int(lines[0].split(" ")[1])
+        book_scores = list(map(int, lines[1].strip().split(" ")))
 
         start_lib_def = 2
-        for i_lib in range(num_librairies):
+        for i_lib in range(num_libraries):
             line_num = start_lib_def + (i_lib * 2)
-            num_book, sign_up_time, ship_per_day = lines[line_num].split() 
-            books_in_lib = set(lines[line_num + 1].split())
-            librairies.append(Librairy(i_lib, sign_up_time, ship_per_day, books_in_lib))
-            
-    return librairies, book_scores
+            num_book, sign_up_time, ship_per_day = list(map(int, lines[line_num].split()))
+            books_in_lib = set(map(int, lines[line_num + 1].split()))
+            libraries.append(
+                Library(i_lib, sign_up_time, ship_per_day, books_in_lib)
+            )
+
+    return libraries, book_scores
 
 
+if __name__ == '__main__':
+    libs, bs = read_input("data/a_example.txt")
+    print(bs)
+    for lib in libs:
+        print(lib)
 
-libs, bs = read_input("data/a_example.txt")
-print(bs)
-for l in libs:
-    print(l)
-
-# print(bs)
+    # print(bs)
